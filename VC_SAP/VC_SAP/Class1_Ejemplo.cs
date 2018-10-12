275bea9df7f65bae393210be983ca7d6e0992435
Adeir Modragon Aguirre <ing.adeir@gmail.com>
2018-10-12 13:10:29 -0500

    Carga_proyecto_Master

---
+++ VC_SAP/VC_SAP/Class1_Ejemplo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VC_SAP
+{
+    class Class1_Ejemplo
+    {
+        string va = "hola";
+    }
+}
